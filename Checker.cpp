ebac6e7ee2ddcab16e496865b8a17359e05a08a7
vathsaaaa <m.srivathsa@in.bosch.com>
2025-08-08 09:40:09 +0530

    reduce complexity

--- Checker.cpp
+++ Checker.cpp
@@ -1,22 +1,43 @@
 #include <assert.h>
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <stdbool.h>
 
+// Individual checks
+bool isTemperatureOk(float temperature) {
+    if (temperature < 0 || temperature > 45) {
+        printf("Temperature out of range!\n");
+        return false;
+    }
+    return true;
+}
+
+bool isSocOk(float soc) {
+    if (soc < 20 || soc > 80) {
+        printf("State of Charge out of range!\n");
+        return false;
+    }
+    return true;
+}
+
+bool isChargeRateOk(float chargeRate) {
+    if (chargeRate > 0.8) {
+        printf("Charge Rate out of range!\n");
+        return false;
+    }
+    return true;
+}
+
+// Composed check
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  return true;
+    bool tempOk = isTemperatureOk(temperature);
+    bool socOk = isSocOk(soc);
+    bool chargeRateOk = isChargeRateOk(chargeRate);
+
+    return tempOk && socOk && chargeRateOk;
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+    assert(batteryIsOk(25, 70, 0.7) == true);
+    assert(batteryIsOk(50, 85, 0) == false);
+    return 0;
 }
